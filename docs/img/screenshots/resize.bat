f39ed78468eb4b79b916262686f3de37a6bef107
if1live <libsora25@gmail.com>
2017-06-14 16:44:48 +0900

    큐브 위치 흩어지게 배치 가능하도록

--- docs/img/screenshots/resize.bat
+++ docs/img/screenshots/resize.bat
@@ -1,9 +1,9 @@
+magick sc01.png -resize 360 resize_01.jpg
 magick sc02.png -resize 360 resize_02.jpg
 magick sc03.png -resize 360 resize_03.jpg
 magick sc04.png -resize 360 resize_04.jpg
-magick sc05.png -resize 360 resize_05.jpg
 
+magick sc01.png -resize 720 resize_large_01.jpg
 magick sc02.png -resize 720 resize_large_02.jpg
 magick sc03.png -resize 720 resize_large_03.jpg
 magick sc04.png -resize 720 resize_large_04.jpg
-magick sc05.png -resize 720 resize_large_05.jpg
