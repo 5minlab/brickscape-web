268d96cf23b398535809c04feccafb8c40cf9fd2
if1live <libsora25@gmail.com>
2017-06-14 14:37:42 +0900

    스크린샷 크기 조정

--- docs/img/screenshots/resize.bat
+++ docs/img/screenshots/resize.bat
@@ -1,12 +1,9 @@
-magick sc01.png -resize 720 resize_01.jpg
+magick sc02.png -resize 360 resize_02.jpg
+magick sc03.png -resize 360 resize_03.jpg
+magick sc04.png -resize 360 resize_04.jpg
+magick sc05.png -resize 360 resize_05.jpg
 
-magick sc02.png -resize 480 resize_02.jpg
-magick sc03.png -resize 480 resize_03.jpg
-magick sc04.png -resize 480 resize_04.jpg
-magick sc05.png -resize 480 resize_05.jpg
-
-magick convert sc01.png resize_large_01.jpg
-magick convert sc02.png resize_large_02.jpg
-magick convert sc03.png resize_large_03.jpg
-magick convert sc04.png resize_large_04.jpg
-magick convert sc05.png resize_large_05.jpg
+magick sc02.png -resize 720 resize_large_02.jpg
+magick sc03.png -resize 720 resize_large_03.jpg
+magick sc04.png -resize 720 resize_large_04.jpg
+magick sc05.png -resize 720 resize_large_05.jpg
