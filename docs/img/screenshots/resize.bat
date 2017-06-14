d702b7d075ec366c57b4bd506209bb3c3ffb2c8a
if1live <libsora25@gmail.com>
2017-06-14 17:14:15 +0900

    폰트 교체. 기준 디자인은 noto sans thin, medium 이지만 이건 한글에만 있는 폰트다. 영어용은 regular, bold로 대신 써야겠다 투명 큐브 이미지 추가. 좌표 노가다는 아직

--- docs/img/screenshots/resize.bat
+++ docs/img/screenshots/resize.bat
@@ -1,7 +1,7 @@
-magick sc01.png -resize 360 resize_01.jpg
-magick sc02.png -resize 360 resize_02.jpg
-magick sc03.png -resize 360 resize_03.jpg
-magick sc04.png -resize 360 resize_04.jpg
+magick cube_transp1.png -resize 360 cube-resize_01.png
+magick cube_transp2.png -resize 360 cube-resize_02.png
+magick cube_transp3.png -resize 360 cube-resize_03.png
+magick cube_transp4.png -resize 360 cube-resize_04.png
 
 magick sc01.png -resize 720 resize_large_01.jpg
 magick sc02.png -resize 720 resize_large_02.jpg
